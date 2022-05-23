eaf4a41aa8d10fe0679af5db6cabd127c853784a
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2022-05-22 22:42:08 -0300

    Projeto inicial

---
+++ Calculo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Calculo
+    {
+        public Calculo()
+        {
+        }
+
+        public Task<List<int>> listaNumeros()
+        {
+            var random = new Random(); // numeros aleatorios
+            var list = Enumerable.Range(0, 100).Select(number => random.Next(0, 100)).ToList();
+            return Task.Run(() => list);
+        }
+
+        public async void imprimirNumeros()
+        {
+            var list = await listaNumeros();
+            var listaFinal = list.Where(list => list <= 10).OrderBy(list => list).Distinct();
+            Thread.Sleep(5000);
+            foreach (var l in listaFinal)
+            {
+                Console.WriteLine(l.ToString(), ConsoleColor.Blue);
+            }
+            Console.WriteLine("--------------------------------------------------", ConsoleColor.Green);
+            Console.WriteLine("Terminei o Trabalho", ConsoleColor.Red);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Calculo c = new Calculo();
+                Console.WriteLine($"Chamei o método { nameof(c.imprimirNumeros)}...", ConsoleColor.Yellow);
+                c.imprimirNumeros();
+                //assicrono
+                //throw new Exception("Erro");
+                Console.WriteLine("Pronto, Tudo deu certo! \nQuando eu terminar te aviso...", ConsoleColor.Green);
+                Console.WriteLine("--------------------------------------------------", ConsoleColor.Green);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
